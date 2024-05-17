ec92c08a86944e8ba4332a9c5ebe823dd8316c83
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-17 08:52:35 +0530

    800

---
+++ A_Maximize.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Vaibhav cout.tie(NULL);
+#define nt <<"\n";
+#define yes cout << "YES" nt
+#define no cout << "NO" nt
+const long double pi = 3.141592653589793238;
+const long long INF = LONG_LONG_MAX;
+const long long mod = 1000000007;
+int gcd(long long a, long long b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
+int lcm(long long a, long long b){return (a/gcd(a,b)*b);}
+
+
+void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai() {
+    int x; cin >> x;
+    cout << x - 1 nt
+}
+
+signed main () {
+    Code By Vaibhav
+    int Bhai_Test_Case_Hai_Ya; cin >> Bhai_Test_Case_Hai_Ya; while (Bhai_Test_Case_Hai_Ya-- > 0)
+        Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
+
+    return 0;
+}
