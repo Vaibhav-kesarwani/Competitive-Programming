4ed4740c2dfc309190624a0ff724ba9db680bbe2
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-18 08:54:18 +0530

    800

--- A_Calculating_Function.cpp
+++ A_Calculating_Function.cpp
@@ -8,21 +8,18 @@ using namespace std;
 #define yes cout << "YES" nt
 #define no cout << "NO" nt
 const long double pi = 3.141592653589793238;
-const long long INF = LONG_LONG_MAX;
-const long long mod = 1000000007;
-int gcd(long long a, long long b){if (b == 0)return a;return gcd(b, a % b);}
-int lcm(long long a, long long b){return (a/gcd(a,b)*b);}
+const int INF = LONG_LONG_MAX;
+const int mod = 1000000007;
+int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+int lcm(int a, int b){return (a/gcd(a,b)*b);}
 
 
 void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai() {
-    int x; cin >> x;
-    int ans = 0;
-    for (int i = 1; i <= x; i++) {
-        if (i % 2 == 0) ans += i;
-        else ans -= i;
-    }
-
-    cout << ans << "\n";
+    int n, a;
+    cin >> n;
+    if(n % 2 == 0) a = n / 2;
+    else a = ((n + 1) / 2) * (-1);
+    cout << a << endl;
 }
 
 signed main () {
