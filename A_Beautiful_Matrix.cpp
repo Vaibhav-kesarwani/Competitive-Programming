020612ab73cfe2714e561aabe5588d9626a4a122
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-22 18:44:21 +0530

    CP

---
+++ A_Beautiful_Matrix.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(0);
+#define Vaibhav cout.tie(0);
+#define nt << "\n";
+#define yes cout << "YES" nt
+#define no cout << "NO" nt
+const long double pi = 3.141592653589793238;
+const int INF = LONG_LONG_MAX;
+const int mod = 1000000007;
+int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+int lcm(int a, int b){return (a/gcd(a,b)*b);}
+
+
+void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai() {
+    int a[5][5];
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> a[i][j];
+            if (a[i][j] == 1) {
+                cout << abs(i - 2) + abs(j - 2) nt
+                return;
+            }
+        }
+    }
+
+}
+
+signed main () {
+    Code By Vaibhav
+    // int Bhai_Test_Case_Hai_Ya; cin >> Bhai_Test_Case_Hai_Ya; while (Bhai_Test_Case_Hai_Ya-- > 0)
+        Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
+
+    return 0;
+}
