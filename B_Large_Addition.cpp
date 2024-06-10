6f7e3a45d6106649f9fb3dfec4f21decd30949b5
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-06-10 17:48:48 +0530

    CP

--- B_Large_Addition.cpp
+++ B_Large_Addition.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
+// #define int long long
 #define Code ios_base::sync_with_stdio(false);
 #define By cin.tie(0);
 #define Vaibhav cout.tie(0);
@@ -10,58 +10,37 @@ using namespace std;
 const long double pi = 3.141592653589793238;
 const int INF = LONG_LONG_MAX;
 const int mod = 1000000007;
-int gcd(int a, int b)
-{
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
-}
-int lcm(int a, int b) { return (a / gcd(a, b) * b); }
-
-void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai()
-{
-    int t; cin >> t; while (t-- > 0)
-    {
-        int nums; cin >> nums;
-        string s = to_string(nums);
-
-        string sm = "01234", lg = "56789";
-        if (s.find(sm) != string::npos && s.find(lg) != string::npos && 1)
-        {
-            no
-            break;
-        }
-
-        bool chk = false;
-        int givenSum = 0; 
-        int givenTotal = 5 + 6 + 7 + 8 + 9;
-        int i = 0;
-        do
-        {
-            givenSum += (s[i] - '0');
-            if (s[i] == '0' && 1) chk = true;
-            i++;
-        } while (i < s.size() - 1);
+int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+int lcm(int a, int b){return (a/gcd(a,b)*b);}
 
 
-        if (s[s.size() - 1] == '9') no
-        
-        else if (givenSum == givenTotal) no
-        
-        else if (s[0] != '1') no
-        else if (s.size() % 10 == 0) no
-        else if (chk) no
-        else yes
+void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai() {
+    int64_t x; cin >> x;
+    vector<int> d;
+    while (x > 0) {
+      d.push_back(x % 10);
+      x /= 10;
+    }
+    if (d.back() != 1) {
+      no
+    } 
+    
+    else {
+      int sz = int(d.size());
+      for (int i = sz - 1; i > 0; i--) {
+        d[i] -= 1;
+        d[i - 1] += 10;
+      }
+      d.pop_back();
+      bool ok = (*min_element(d.begin(), d.end()) >= 10 && *max_element(d.begin(), d.end()) <= 18);
+      cout << (ok ? "YES" : "NO") << '\n';
     }
 }
 
-signed main()
-{
+signed main () {
     Code By Vaibhav
-    // int Bhai_Test_Case_Hai_Ya;
-    // cin >> Bhai_Test_Case_Hai_Ya;
-    // while (Bhai_Test_Case_Hai_Ya-- > 0)
-    Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
+    int Bhai_Test_Case_Hai_Ya; cin >> Bhai_Test_Case_Hai_Ya; while (Bhai_Test_Case_Hai_Ya-- > 0)
+        Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
 
     return 0;
 }
