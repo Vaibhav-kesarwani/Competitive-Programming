bfbe0b040423aec30e576d873593b3c6c9bdebcc
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-06-13 20:23:27 +0530

    CP

---
+++ A_power_B_mod_C.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    // your code goes here
+
+    int t;
+
+    cin >> t;
+
+    while (t--)
+    {
+
+        long long a;
+
+        cin >> a;
+
+        long long b, c;
+
+        if (a == 2)
+        {
+
+            cout << 4 << " " << 8 << endl;
+        }
+
+        else
+        {
+
+            cout << 2 * a << " " << a * a << endl;
+        }
+    }
+}
