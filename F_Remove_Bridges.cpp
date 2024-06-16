be098bde5988933eb42bb4a7a1185ed8c89729f7
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-06-16 11:13:31 +0530

    CP

--- F_Remove_Bridges.cpp
+++ F_Remove_Bridges.cpp
@@ -1,61 +1,54 @@
-#include <bits/stdc++.h>
-
-using i64 = long long;
-
-void solve() {
-    int n;
-    std::cin >> n;
-    
-    std::vector<std::vector<int>> adj(n);
-    for (int i = 1; i < n; i++) {
-        int u, v;
-        std::cin >> u >> v;
-        u--, v--;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    
-    std::vector<int> a;
-    auto dfs = [&](auto &&self, int x, int p) -> int {
-        int h = 0;
-        for (auto y : adj[x]) {
-            if (y == p) {
-                continue;
-            }
-            int v = self(self, y, x) + 1;
-            if (v > h) {
-                std::swap(v, h);
-            }
-            if (v > 0) {
-                a.push_back(v);
-            }
-        }
-        return h;
-    };
-    a.push_back(dfs(dfs, 0, -1));
-    
-    std::sort(a.begin(), a.end(), std::greater());
-    
-    int ans = n - 1;
-    for (int i = 1, j = 0; i < n; i++) {
-        while (j < 2 * i - 1 && j < a.size()) {
-            ans -= a[j];
-            j++;
-        }
-        std::cout << ans << " \n"[i == n - 1];
-    }
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cassert>
+using namespace std;
+int N;
+vector<int>G[3<<17];
+vector<int>P;
+int dfs(int u,int p)
+{
+	int mx=0;
+	for(int v:G[u])if(v!=p)
+	{
+		int t=dfs(v,u)+1;
+		if(mx<t)
+		{
+			if(mx>0)P.push_back(mx);
+			mx=t;
+		}
+		else P.push_back(t);
+	}
+	return mx;
 }
-
-int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    
-    int t;
-    std::cin >> t;
-    
-    while (t--) {
-        solve();
-    }
-    
-    return 0;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int T;cin>>T;
+	for(;T--;)
+	{
+		cin>>N;
+		for(int i=0;i<N;i++)G[i].clear();
+		for(int i=1;i<N;i++)
+		{
+			int u,v;cin>>u>>v;u--,v--;
+			G[u].push_back(v);
+			G[v].push_back(u);
+		}
+		P.clear();
+		int F=dfs(0,-1);
+		sort(P.begin(),P.end());
+		reverse(P.begin(),P.end());
+		int ans=N-1-F;
+		int t=0;
+		for(int i=1;i<N;i++)
+		{
+			cout<<ans<<(i+1==N?"\n":" ");
+			for(int _=0;_<2;_++)
+			{
+				if(t<P.size())ans-=P[t++];
+			}
+		}
+	}
 }
