a18a09aadc071873650f57181fcddc1e68e55b7c
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-06-17 20:53:42 +0530

    CP

---
+++ B_Multiply_by_2_divide_by_6.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(0);
+#define Vaibhav cout.tie(0);
+#define nt << "\n";
+#define yes cout << "YES" nt
+#define no cout << "NO" nt
+const long double pi = 3.141592653589793238;
+const int INF = LONG_LONG_MAX;
+const int mod = 1000000007;
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+int lcm(int a, int b) { return (a / gcd(a, b) * b); }
+
+void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai()
+{
+    int n; cin >> n;
+    int num = n, cnt = 0;
+    while (num != 1)
+    {
+        if (num % 6 == 0)
+            num /= 6, cnt++;
+        else if (num % 3 == 0)
+            num *= 2, cnt++;
+        else
+        {
+            cnt = -1;
+            break;
+        }
+    }
+    cout << cnt nt
+}
+
+signed main()
+{
+    Code By Vaibhav int Bhai_Test_Case_Hai_Ya;
+    cin >> Bhai_Test_Case_Hai_Ya;
+    while (Bhai_Test_Case_Hai_Ya-- > 0)
+        Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
+
+    return 0;
+}
