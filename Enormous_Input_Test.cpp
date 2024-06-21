fce90535d9d46ed491bafff1a43a834a43c50e61
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-06-21 17:47:52 +0530

    CP

---
+++ Enormous_Input_Test.cpp
@@ -0,0 +1,27 @@
+// We have populated the solutions for the 10 easiest problems for your support.
+// Click on the SUBMIT button to make a submission to this problem.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n,k;
+    cin >> n >> k;
+
+    int count=0;
+    for (int i=0;i<=n-1;i++)
+    {
+        int a;
+        cin >> a;
+        if (a%k==0)
+        {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
