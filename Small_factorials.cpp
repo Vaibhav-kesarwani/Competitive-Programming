53e5da943743cf87b54151e1e69f6bb928171705
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-06-29 17:00:01 +0530

    CP

---
+++ Small_factorials.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(0);
+#define Vaibhav cout.tie(0);
+#define nt << "\n";
+#define yes cout << "YES" nt
+#define no cout << "NO" nt
+const long double pi = 3.141592653589793238;
+const int INF = LONG_LONG_MAX;
+const int mod = 1000000007;
+int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+int lcm(int a, int b){return (a/gcd(a,b)*b);}
+
+
+void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai() {
+    int n; cin >> n;
+    vector<int>ans;
+    ans.push_back(1);
+    int carry=0;
+    int x, digit;
+    
+    for(int i=2;i<=n;i++){
+        for(int j=0;j<ans.size();j++){
+            x=ans[j]*i+carry;
+            ans[j]=x%10;
+            carry=x/10;
+        }
+        
+        while(carry){
+            ans.push_back(carry%10);
+            carry=carry/10;
+        }
+    }
+    
+    reverse(ans.begin(),ans.end());
+
+    for(auto i:ans){
+        cout<<i;
+    }
+
+    cout nt
+}
+
+signed main () {
+    Code By Vaibhav
+    int Bhai_Test_Case_Hai_Ya; cin >> Bhai_Test_Case_Hai_Ya; while (Bhai_Test_Case_Hai_Ya-- > 0)
+        Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
+
+    return 0;
+}
