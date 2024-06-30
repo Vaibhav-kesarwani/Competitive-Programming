3b7670f597ba96615f0ce14c47ee03cc333dc375
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-06-30 10:19:28 +0530

    CP

---
+++ The_Last_Levels.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(0);
+#define Vaibhav cout.tie(0);
+#define nt << "\n";
+#define yes cout << "YES" nt
+#define no cout << "NO" nt
+const long double pi = 3.141592653589793238;
+const int INF = LONG_LONG_MAX;
+const int mod = 1000000007;
+int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+int lcm(int a, int b){return (a/gcd(a,b)*b);}
+
+
+void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai() {
+    int x, y, z; cin >> x >> y >> z;
+    if (x <= 3) cout << x * y nt
+    else if (x % 3 == 0) cout << (((x / 3) - 1) * z) + (x * y) nt
+    else cout << ((x / 3) * z) + (x * y) nt
+}
+
+signed main () {
+    Code By Vaibhav
+    int Bhai_Test_Case_Hai_Ya; cin >> Bhai_Test_Case_Hai_Ya; while (Bhai_Test_Case_Hai_Ya-- > 0)
+        Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
+
+    return 0;
+}
