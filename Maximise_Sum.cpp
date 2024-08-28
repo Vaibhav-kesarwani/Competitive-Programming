e2f094a65dee2e3d5d1d45ee6e74d78400072126
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-08-28 22:28:53 +0530

    CP

---
+++ Maximise_Sum.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(0);
+#define Vaibhav cout.tie(0);
+#define nt << "\n";
+#define yes cout << "YES"
+#define no cout << "NO"
+#define Yes cout << "Yes"
+#define No cout << "No"
+const long double pi = 3.141592653589793238;
+const int INF = LONG_LONG_MAX;
+const int mod = 1000000007;
+int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+int lcm(int a, int b){return (a/gcd(a,b)*b);}
+
+
+void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai() {
+    int n; cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+
+    int cnt = 0, sum = 0, mini = INF;
+
+    for (int it : v) {
+        if (it < 0) cnt++;
+        sum += abs(it);
+        mini = min(mini, abs(it));
+    }
+
+    cout << (cnt % 2 == 0 ? sum : sum - 2 * mini) nt
+}
+
+signed main () {
+    Code By Vaibhav
+    int Bhai_Test_Case_Hai_Ya; cin >> Bhai_Test_Case_Hai_Ya; while (Bhai_Test_Case_Hai_Ya-- > 0)
+        Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
+
+    return 0;
+}
