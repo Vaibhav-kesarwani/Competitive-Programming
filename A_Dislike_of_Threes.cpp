4b5d723c8937128f06e264c207d8de3d6231fc5d
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-09-04 08:38:22 +0530

    CP

---
+++ A_Dislike_of_Threes.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(0);
+#define Vaibhav cout.tie(0);
+#define nt << "\n";
+#define yes cout << "YES"
+#define no cout << "NO"
+#define Yes cout << "Yes"
+#define No cout << "No"
+const long double pi = 3.141592653589793238;
+const int INF = LONG_LONG_MAX;
+const int mod = 1000000007;
+int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+int lcm(int a, int b){return (a/gcd(a,b)*b);}
+
+
+void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai() {
+    int arr[1001];
+
+    int j = 1;
+    for(int i = 1 ; i <= 1666 ; i++ ){
+        if(i%3 != 0)
+        {
+            if(i%10 != 3){
+                arr[j] = i;
+                j = j + 1;
+            }
+
+        }
+
+    }
+
+    int t;
+    cin>>t;
+    int k;
+    while(t--){
+        cin>>k;
+        cout<<arr[k]<<endl;
+    }
+
+}
+
+signed main () {
+    Code By Vaibhav
+    // int Bhai_Test_Case_Hai_Ya; cin >> Bhai_Test_Case_Hai_Ya; while (Bhai_Test_Case_Hai_Ya-- > 0)
+        Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
+
+    return 0;
+}
