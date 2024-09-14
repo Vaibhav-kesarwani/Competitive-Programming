eb9a84399933fd1ab5fe534afd6080872eeaa1a2
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-09-14 09:01:24 +0530

    CP

---
+++ A_Black_Square.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(0);
+#define Vaibhav cout.tie(0);
+#define nt << "\n";
+#define yes cout << "YES"
+#define no cout << "NO"
+#define Yes cout << "Yes"
+#define No cout << "No"
+const long double pi = 3.141592653589793238;
+const int INF = LONG_LONG_MAX;
+const int mod = 1000000007;
+int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+int lcm(int a, int b){return (a/gcd(a,b)*b);}
+
+
+void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai() {
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    string s; cin >> s;
+    int ans = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '1') ans += a;
+        else if (s[i] == '2') ans += b;
+        else if (s[i] == '3') ans += c;
+        else ans += d;
+    }
+    cout << ans nt;
+}
+
+signed main () {
+    Code By Vaibhav
+    // int Bhai_Test_Case_Hai_Ya; cin >> Bhai_Test_Case_Hai_Ya; while (Bhai_Test_Case_Hai_Ya-- > 0)
+        Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
+
+    return 0;
+}
