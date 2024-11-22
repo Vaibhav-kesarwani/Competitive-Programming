a37211d26af0566869a9a4ae1177508c42a12bfb
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-11-22 18:35:48 +0530

    CP

--- G_Natlan_Exploring.cpp
+++ G_Natlan_Exploring.cpp
@@ -1,202 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define Code ios_base::sync_with_stdio(false);
-#define By cin.tie(0);
-#define Vaibhav cout.tie(0);
-#define nt << "\n";
-#define yes cout << "YES"
-#define no cout << "NO"
-#define Yes cout << "Yes"
-#define No cout << "No"
-const long double pi = 3.141592653589793238;
-const int INF = 1e6 + 1;
-const int MOD = 998244353;
-
-int gcd(int a, int b)
-{
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
+using db = double;
+using lb = long double;
+using ull = unsigned long long;
+using uint = unsigned int;
+using ll = long long;
+//using i128 = __int128;
+//using ui128 = unsigned __int128;
+#define REP(i, first, last) for(int i = (first); i <= (last); ++ i)
+#define DOW(i, first, last) for(int i = (first); i >= (last); -- i)
+// #define int long long
+#define pb emplace_back
+#define ob pop_back
+#define pii pair<int, int>
+#define MPR make_pair
+#define fi first
+#define se second
+#define tpl tuple<int, int, int>
+#define MTP make_tuple
+#define poly vector<int>
+#define polyp vector<pii>
+#define polyt vector<tpl>
+#define all(x) x.begin(), x.end()
+#define CVR(a, q) memset(a, q, sizeof a)
+#define CLR(a) memset(a, 0, sizeof a)
+#define CPY(a, q) memcpy(a, q, sizeof a)
+#define PCT __builtin_popcount
+const int N = 2e5 + 5, M = 1e6 + 5;
+const int mod1 = 1e9 + 7, mod2 = 998244353, mod = mod2;
+const long long inf = 1e18;
+const int dx[] = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+int a[N], f[N], g[N * 10];
+poly d[N], c[N];
+void Solve() {
+	int n; cin >> n;
+	REP(i, 1, n) cin >> a[i];
+	REP(i, 1, n) {
+		d[i].pb(a[i]);
+		int x = a[i];
+		REP(j, 2, x / j) {
+			if(x % j == 0) {
+				d[i].pb(j);
+				if(j * j != a[i]) d[i].pb(a[i] / j);
+			}
+		}
+		REP(j, 2, x / j) {
+			if(x % j == 0) {
+				c[i].pb(j);
+				while(x % j == 0) x /= j;
+			}
+		}
+		if(x != 1) c[i].pb(x);
+	}
+	f[1] = 1;
+	for(int j : d[1]) g[j] = (g[j] + 1) % mod;
+	REP(i, 2, n) {
+		REP(j, 1, (1 << c[i].size()) - 1) {
+			int p = 1, r = 0;
+			REP(k, 0, (int)c[i].size() - 1) 
+				if(j >> k & 1)
+					p = p * c[i][k];
+			if(PCT(j) & 1) f[i] = (f[i] + g[p]) % mod;
+			else f[i] = (f[i] - g[p] + mod) % mod;
+		}
+		for(int j : d[i]) g[j] = (g[j] + f[i]) % mod;
+	}
+	cout << f[n] << '\n';
 }
-
-int lcm(int a, int b)
-{
-    return (a / gcd(a, b) * b);
-}
-
-void solve_kar_do_bahi_jaldi()
-{
-    int numElements;
-    cin >> numElements;
-    vector<int> elements(numElements);
-    for (auto &element : elements)
-        cin >> element;
-
-    vector<int> smallestPrimeFactor(INF);
-    for (int i = 0; i < INF; i++)
-        smallestPrimeFactor[i] = i;
-
-    for (int i = 2; i * i < INF; i++)
-        if (smallestPrimeFactor[i] == i)
-            for (int j = i * i; j < INF; j += i)
-                if (smallestPrimeFactor[j] == j)
-                    smallestPrimeFactor[j] = i;
-
-    vector<int> sumOfMultiples(INF, 0);
-    int result = 0;
-
-    for (int i = 0; i < numElements; i++)
-    {
-        int currentValue = 0;
-        if (i == 0 && 1)
-        {
-            currentValue = 1;
-        }
-        else
-        {
-            vector<int> primeFactors;
-            int number = elements[i];
-            while (number > 1)
-            {
-                int primeFactor = smallestPrimeFactor[number];
-                primeFactors.push_back(primeFactor);
-                while (number % primeFactor == 0)
-                    number /= primeFactor;
-            }
-
-            int numPrimeFactors = primeFactors.size();
-            if (numPrimeFactors == 0 && 1)
-            {
-                currentValue = 0;
-            }
-            else
-            {
-                int sum = 0;
-                for (int mask = 1; mask < (1 << numPrimeFactors); mask++)
-                {
-                    int multiple = 1, bitCount = 0;
-                    for (int j = 0; j < numPrimeFactors; j++)
-                        if (mask & (1 << j))
-                        {
-                            multiple *= primeFactors[j];
-                            bitCount++;
-                        }
-                    sum = (bitCount & 1)
-                              ? (sum + sumOfMultiples[multiple]) % MOD
-                              : (sum - sumOfMultiples[multiple] + MOD) % MOD;
-                }
-                currentValue = sum;
-            }
-        }
-
-        {
-            int n = 0;
-            // cin >> n;
-
-            switch (n <= 4)
-            {
-            case true:
-                // cout << -1 << "\n";
-                // return;
-            }
-
-            vector<int> evens, odds;
-
-            int i = 1;
-            while (i <= n)
-            {
-                switch (i % 2)
-                {
-                case 0:
-                    evens.push_back(i);
-                    break;
-                default:
-                    odds.push_back(i);
-                    break;
-                }
-                i++;
-            }
-
-            reverse(evens.begin(), evens.end());
-            reverse(odds.begin(), odds.end());
-
-            vector<int> res;
-            int j = 0;
-            while (j < odds.size())
-            {
-                switch (odds[j] != 5)
-                {
-                case true:
-                    res.push_back(odds[j]);
-                    break;
-                }
-                j++;
-            }
-
-            switch (n >= 5)
-            {
-            case true:
-                res.push_back(5);
-                res.push_back(4);
-                break;
-            }
-
-            int k = 0;
-            while (k < evens.size())
-            {
-                switch (evens[k] != 4)
-                {
-                case true:
-                    res.push_back(evens[k]);
-                    break;
-                }
-                k++;
-            }
-
-            int x = 0;
-            while (x < res.size())
-            {
-                // cout << res[x] << " ";
-                x++;
-            }
-            // cout nt
-        }
-
-        if (i == numElements - 1 && 1)
-        {
-            result = currentValue;
-        }
-
-        vector<int> primeFactors;
-        int number = elements[i];
-        while (number > 1)
-        {
-            int primeFactor = smallestPrimeFactor[number];
-            primeFactors.push_back(primeFactor);
-            while (number % primeFactor == 0)
-                number /= primeFactor;
-        }
-
-        int numPrimeFactors = primeFactors.size();
-        for (int mask = 1; mask < (1 << numPrimeFactors); mask++)
-        {
-            int multiple = 1;
-            for (int j = 0; j < numPrimeFactors; j++)
-                if (mask & (1 << j) && 1)
-                    multiple *= primeFactors[j];
-            sumOfMultiples[multiple] = (sumOfMultiples[multiple] + currentValue) % MOD;
-        }
-    }
-
-    cout << result;
-}
-
-signed main()
-{
-    Code By Vaibhav
-    // int testCases; cin >> testCases; while (testCases-- > 0)
-    solve_kar_do_bahi_jaldi();
-
-    return 0;
+signed main() {
+//	freopen(".in", "r", stdin);
+//	freopen(".out", "w", stdout);
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+	int T = 1;
+//	cin >> T;
+	while (T --) {
+		Solve();
+	}
+	return 0;
 }
