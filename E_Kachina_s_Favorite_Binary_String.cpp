ddaa9069568d7ca128a58f29a392c48d8443faa9
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-11-22 18:37:05 +0530

    CP

--- E_Kachina_s_Favorite_Binary_String.cpp
+++ E_Kachina_s_Favorite_Binary_String.cpp
@@ -1,94 +1,120 @@
+#include <iostream>
+#include <numeric>
 #include <bits/stdc++.h>
+
 using namespace std;
-#define int long long
-#define Code ios_base::sync_with_stdio(false);
-#define By cin.tie(0);
-#define Vaibhav cout.tie(0);
-#define nt << "\n";
-#define mt cout << "! IMPOSSIBLE"
-#define no cout << "NO"
-#define Yes cout << "Yes"
-#define No cout << "No"
-const long double pi = 3.141592653589793238;
-const int INF = LONG_LONG_MAX;
-const int mod = 1000000007;
-int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
-int lcm(int a, int b){return (a/gcd(a,b)*b);}
 
-int help(int l, int r) {
-    cout << "? " << l << " " << r nt
-    cout.flush();
-    int y;
-    cin >> y;
-    return y;
-}
+#define ll                   long long
+#define pp                   pop_back
+#define pb                   push_back
+#define cy                   cout<< "YES\n";
+#define cn                   cout<< "NO\n";
+#define fast                 ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define FOR(i,n)             for(ll i=1;i<=n;i++)
+#define LOOP(i,n)            for(ll i=0;i<n;i++)
+#define FOOR(i,a,b)          for(ll i=a;i<=b;i++)
+#define LOP(i,a,b)           for(ll i=a;i<b;i++)
+#define inf                  100000
+#define MAXN                 100004
+const ll INF = LONG_LONG_MAX;
+const int IINF = 1e9 + 5;
+const ll mod = 1e9 + 7;
+// (a / b) % mod ->  (a % mod)* binpow(b, mod - 2) % mod
+//((p % mod) + mod) % mod // negative mod
+//ll vis[MAXN], vis1[MAXN], lvl[MAXN] = {-1};
+//bool bfs(ll src, ll n, vector<ll> graph[])
+//{
+//    queue<ll> q;
+//    int color[n + 1];
+//    for(ll i = 0; i <= n; i++)
+//        color[i] = -1;
+//    color[src] = 1;
+//    q.push(src);
+//    while(!q.empty())
+//    {
+//        ll temp = q.front();
+//        q.pop();
+//        for(ll i = 0; i < graph[temp].size(); i++)
+//        {
+//            if(graph[temp][i] == temp)
+//                return false;
+//            else if(color[graph[temp][i]] == -1)
+//            {
+//                color[graph[temp][i]] = 1 - color[temp];
+//                q.push(graph[temp][i]);
+//            }
+//            else if(color[graph[temp][i]] == color[temp])
+//            {
+//                return false;
+//            }
+//        }
+//    }
+//    return true;
+//}
 
-void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai() {
+//bool cmp(pair<pair<int, int>, char> a, pair<pair<int, int>, char> b)
+//{
+//    if(a.first.first == b.first.first)
+//    {
+//        return a.first.second > b.first.second;
+//    }
+//    else
+//        return a.first < b.first;
+//}
+
+void solve()
+{
     int n;
     cin >> n;
-    string ans;
-    int prev = 0;
-    int cnt0 = 0;
-    int i = 1;
-    while (i < n) {
-        int x = help(1, i + 1);
-        if (prev == 0) {
-            if (x == 0) {
-                i++;
-                continue;
-            } else {
-                if (x > i) {
-                    mt nt
-                    return;
-                } else {
-                    int j = 0;
-                    while (j < i - x) {
-                        ans.push_back('1');
-                        j++;
-                    }
-                    j = 0;
-                    while (j < x) {
-                        ans.push_back('0');
-                        j++;
-                    }
-                }
-            }
-            ans.push_back('1');
-            cnt0 = x;
-            prev = x;
-        } else {
-            if (x > prev) {
-                if (x - prev == cnt0) {
-                    ans.push_back('1');
-                    prev = x;
-                } else {
-                    mt nt
-                    return;
-                }
-            } else if (x == prev) {
-                ans.push_back('0');
-                cnt0++;
-            } else {
-                mt nt
-                return;
-            }
-        }
-        i++;
+    vector<int> ar(n);
+    for(int i = 1; i < n; ++i)
+    {
+        cout << "? " << 1 << " " << i + 1 << "\n";
+        // cout.flush();
+        int res;
+        cin >> res;
+        ar[i] = res;
     }
-    
-    if (ans.size() < n) {
-        mt nt
+    if(ar[n - 1] == 0)
+    {
+        cout << "! IMPOSSIBLE\n";
         return;
     }
-    cout << "! " << ans nt
-    cout.flush();
+    int temp = 1, k;
+    while(ar[temp] == 0)
+    {
+        ++temp;
+    }
+    k = ar[temp];
+    string str(n, '0');
+    for(int i = 0; i < temp - k; ++i)
+    {
+        str[i] = '1';
+    }
+    for(int i = temp; i < n; ++i)
+    {
+        str[i] = (((ar[i] > ar[i - 1]) ? '1' : '0'));
+    }
+    cout << "! " << str << "\n";
 }
 
-signed main () {
-    Code By Vaibhav
-    int Bhai_Test_Case_Hai_Ya; cin >> Bhai_Test_Case_Hai_Ya; 
-    while (Bhai_Test_Case_Hai_Ya-- > 0)
-        Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
+int main()
+{
+    // fast;
+//    sieve1();
+    int t, n;
+//    int c = 1;
+    cin >> t;
+//    t = 1;
+    while(t--)
+    {
+//        cout << "Case " << c << ": ";
+//        c++;
+        //memset(dp, -1, sizeof(dp));
+        solve();
+    }
 
+
+    //cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << " ms\n";
     return 0;
 }
