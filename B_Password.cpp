1c5ae87d3cecd839a077889432382fe85a85e75f
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-11-30 18:20:33 +0530

    CP

--- B_Password.cpp
+++ B_Password.cpp
@@ -1,49 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define ll long long
+#define INF 0x3f3f3f3f
+#define N 200005
+#define mem(arr, val) memset(arr, val, sizeof(arr))
+#define pii pair<int, int>
 using namespace std;
 
-int pi[1000005];
-string p;
+string s;
+int len;
+vector<int> ans, pi;
 
-int KMP(){
-    int now;
-
-    pi[0]= now= -1;
-
-    for(int i=1;i<p.length();i++){
-
-        while(now!=-1 && p[i]!=p[now+1])
-            now= pi[now];
-
-        if(p[i]==p[now+1])
-            pi[i]= ++now;
-        else
-            pi[i]= -1;
-
-
-    }
-
+void pre_fix() {
+    pi.assign(len + 1, 0);
+    for(int i = 1; i < len; i ++) {
+        int j = pi[i - 1];
+        while(j > 0 && s[i] != s[j]) {
+            j = pi[j - 1];
+        }
+        if(s[i] == s[j]) j ++;
+        pi[i] = j; 
+    }    
 }
 
-main(){
-    cin>> p;
-    KMP();
-    int len= p.length();
-
-    int maxi= pi[len-1];
-    int found= pi[maxi];
-    if(maxi==-1)
-        found=-1;
-
-    for(int i=1;i<(len-1);i++){
-        if(pi[i]==maxi)
-            found=maxi;
+void solve(){
+    cin >> s;
+    len = s.size();
+    pre_fix();
+    ans.assign(len + 1, 0);
+    for (int i = 0; i < len; i++) ans[pi[i]]++; 
+    for (int i = len - 1; i > 0; i--) { 
+        ans[pi[i - 1]] += ans[i]; 
     }
-
-    if(found==-1)
-        cout<<"Just a legend"<<endl;
-    else{
-        cout<< p.substr(0,found+1) <<endl;
+    for (int i = 0; i <= len; i++) ans[i]++;
+    int res = 0;
+    int k = pi[len - 1];
+    while(k > 0) {
+        if(ans[k] >= 3) {
+            res = k;
+            break;
+        } else {
+            k = pi[k - 1];
+        }
     }
+    if(res == 0) {
+        cout << "Just a legend";
+    } else {
+        cout << s.substr(0, res);   
+    }
+}
 
 
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    solve();
+      
+    return 0;
 }
