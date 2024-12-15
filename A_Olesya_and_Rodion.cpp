0a6294e6381cb875b0e3840f92850861d59c6d5f
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-12-15 10:04:53 +0530

    CP

--- A_Olesya_and_Rodion.cpp
+++ A_Olesya_and_Rodion.cpp
@@ -1,10 +1,9 @@
 /*
  * Author        :         Vaibhav Kesarwani
- * Created       :         December 15, 2024 09:56:49
+ * Created       :         December 15, 2024 10:03:49
  * Workspace     :         ~/Desktop/Competitive-Programming
  * Problem name  :         A_Olesya_and_Rodion
-*/
-
+ */
 
 // including the files
 #include <bits/stdc++.h>
@@ -18,14 +17,14 @@ const int INF = INT_MAX;
 const int mod = 1000000007;
 
 // different kind of for states
-#define for0(i, n) for (int i = 0; i < (int)(n); ++i) // 0 based indexing
-#define for1(i, n) for (int i = 1; i <= (int)(n); ++i) // 1 based indexing
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)            // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)           // 1 based indexing
 #define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i) // closed interver from l to r r inclusive
-#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) // reverse 0 based.
-#define forr1(i, n) for (int i = (int)(n); i >= 1; --i) // reverse 1 based
-#define fora(a) for(auto i:a) // for the linear traveral
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i)      // reverse 0 based.
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)          // reverse 1 based
+#define fora(a) for (auto i : a)                                 // for the linear traveral
 
-//short hand for usual tokens
+// short hand for usual tokens
 #define pb push_back
 #define fi first
 #define se second
@@ -41,35 +40,40 @@ const int mod = 1000000007;
 #define Vaibhav cout.tie(0);
 
 // to be used with algorithms that processes a container Eg: find(all(c),42)
-#define all(x) (x).begin(), (x).end() //Forward traversal
-#define rall(x) (x).rbegin, (x).rend() //reverse traversal
-#define gcd(a,b) __gcd(a,b)
-#define lcm(a,b) (a*(b/gcd(a,b)))
+#define all(x) (x).begin(), (x).end()  // Forward traversal
+#define rall(x) (x).rbegin, (x).rend() // reverse traversal
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / gcd(a, b)))
 
 // traversal function to avoid long template definition. Now with C++11 auto alleviates the pain.
-#define tr(c,i) for(__typeof__((c)).begin() i = (c).begin(); i != (c).end(); i++)
+#define tr(c, i) for (__typeof__((c)).begin() i = (c).begin(); i != (c).end(); i++)
 
 // find if a given value is present in a container. Container version. Runs in log(n) for set and map
-#define present(c,x) ((c).find(x) != (c).end())
+#define present(c, x) ((c).find(x) != (c).end())
 
-//find version works for all containers. This is present in std namespace.
-#define cpresent(c,x) (find(all(c),x) != (c).end())
+// find version works for all containers. This is present in std namespace.
+#define cpresent(c, x) (find(all(c), x) != (c).end())
 
 // Avoiding wrap around of size()-1 where size is a unsigned int.
 #define sz(a) int((a).size())
 
 // Custom Functions
-int binpow (int a, int n) {
-  if (n == 0) return 1;
-  if (n % 2 == 1) return (a * binpow(a, n / 2)) % mod;
-  else {
-    int temp = binpow(a, n / 2);
-    return (temp * temp) % mod;  
-  }
+int binpow(int a, int n)
+{
+    if (n == 0)
+        return 1;
+    if (n % 2 == 1)
+        return (a * binpow(a, n / 2)) % mod;
+    else
+    {
+        int temp = binpow(a, n / 2);
+        return (temp * temp) % mod;
+    }
 }
 
-int inverse (int x) {
-  return binpow(x, (mod - 2));
+int inverse(int x)
+{
+    return binpow(x, (mod - 2));
 }
 
 // Shorthand for commonly used types
@@ -96,22 +100,31 @@ ll cnt = 0, ans = 0;
 
 // solve function
 void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai() {
-    cin >> n >> x;
-    if (n == 1 && x == 10) {
-        cout << -1 nt
-    } else {
-        cout << x;
-        for (int i = 1; i < n; i++) {
-            cout << 0;
+    long int x, y, n, i, t;
+
+    while (cin >> n >> t) {
+        if (n == 1 && t == 10)
+            cout << "-1\n";
+        else if (n >= 2 && t == 10) {
+            for (i = 1; i < n; i++)
+            {
+                cout << "1";
+            }
+            cout << "0\n";
         }
-        cout nt
+        else {
+            for (i = 1; i <= n; i++) {
+                cout << t;
+            }
+            cout nt
+        }
     }
 }
 
 // main function
-int32_t main () {
-    Code By Vaibhav
-    // int Bhai_Test_Case_Hai_Ya; cin >> Bhai_Test_Case_Hai_Ya; while (Bhai_Test_Case_Hai_Ya-- > 0)
+int32_t main() {
+    Code By Vaibhav 
+    // int Bhai_Test_Case_Hai_Ya; cin >> Bhai_Test_Case_Hai_Ya;while (Bhai_Test_Case_Hai_Y a-- > 0)
         Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
 
     return 0;
