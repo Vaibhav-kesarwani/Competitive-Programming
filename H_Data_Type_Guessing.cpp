419b54186b61924d64248f0c85b8a833bbe7114d
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-12-15 09:50:28 +0530

    CP

--- H_Data_Type_Guessing.cpp
+++ H_Data_Type_Guessing.cpp
@@ -3,8 +3,7 @@
  * Created       :         December 15, 2024 09:39:10
  * Workspace     :         ~/Desktop/Competitive-Programming
  * Problem name  :         H_Data_Type_Guessing
-*/
-
+ */
 
 // including the files
 #include <bits/stdc++.h>
@@ -18,14 +17,14 @@ const int INF = INT_MAX;
 const int mod = 1000000007;
 
 // different kind of for states
-#define for0(i, n) for (int i = 0; i < (int)(n); ++i) // 0 based indexing
-#define for1(i, n) for (int i = 1; i <= (int)(n); ++i) // 1 based indexing
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)            // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)           // 1 based indexing
 #define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i) // closed interver from l to r r inclusive
-#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) // reverse 0 based.
-#define forr1(i, n) for (int i = (int)(n); i >= 1; --i) // reverse 1 based
-#define fora(a) for(auto i:a) // for the linear traveral
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i)      // reverse 0 based.
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)          // reverse 1 based
+#define fora(a) for (auto i : a)                                 // for the linear traveral
 
-//short hand for usual tokens
+// short hand for usual tokens
 #define pb push_back
 #define fi first
 #define se second
@@ -41,35 +40,40 @@ const int mod = 1000000007;
 #define Vaibhav cout.tie(0);
 
 // to be used with algorithms that processes a container Eg: find(all(c),42)
-#define all(x) (x).begin(), (x).end() //Forward traversal
-#define rall(x) (x).rbegin, (x).rend() //reverse traversal
-#define gcd(a,b) __gcd(a,b)
-#define lcm(a,b) (a*(b/gcd(a,b)))
+#define all(x) (x).begin(), (x).end()  // Forward traversal
+#define rall(x) (x).rbegin, (x).rend() // reverse traversal
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / gcd(a, b)))
 
 // traversal function to avoid long template definition. Now with C++11 auto alleviates the pain.
-#define tr(c,i) for(__typeof__((c)).begin() i = (c).begin(); i != (c).end(); i++)
+#define tr(c, i) for (__typeof__((c)).begin() i = (c).begin(); i != (c).end(); i++)
 
 // find if a given value is present in a container. Container version. Runs in log(n) for set and map
-#define present(c,x) ((c).find(x) != (c).end())
+#define present(c, x) ((c).find(x) != (c).end())
 
-//find version works for all containers. This is present in std namespace.
-#define cpresent(c,x) (find(all(c),x) != (c).end())
+// find version works for all containers. This is present in std namespace.
+#define cpresent(c, x) (find(all(c), x) != (c).end())
 
 // Avoiding wrap around of size()-1 where size is a unsigned int.
 #define sz(a) int((a).size())
 
 // Custom Functions
-int binpow (int a, int n) {
-  if (n == 0) return 1;
-  if (n % 2 == 1) return (a * binpow(a, n / 2)) % mod;
-  else {
-    int temp = binpow(a, n / 2);
-    return (temp * temp) % mod;  
-  }
+int binpow(int a, int n)
+{
+    if (n == 0)
+        return 1;
+    if (n % 2 == 1)
+        return (a * binpow(a, n / 2)) % mod;
+    else
+    {
+        int temp = binpow(a, n / 2);
+        return (temp * temp) % mod;
+    }
 }
 
-int inverse (int x) {
-  return binpow(x, (mod - 2));
+int inverse(int x)
+{
+    return binpow(x, (mod - 2));
 }
 
 // Shorthand for commonly used types
@@ -96,23 +100,22 @@ ll cnt = 0, ans = 0;
 
 // solve function
 void Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai() {
-    cin >> x >> y >> z;
+    double n, k, a; cin >> n >> k >> a;
+    
+    double res = (min(n, k) / a) * max(n, k);
+    long long res2 = res;
+    double res3 = res - res2;
 
-    double result = (x * y) / z;
-
-    long long myNumber = result;
-    double myRes = result - myNumber;
-
-    if (myRes > 0) cout << "double" nt
-    else if (myNumber <= 2147483647) cout << "int" nt
-    else cout << "long long" nt
+    if (abs(res3) > 0 && abs(res3) < 1) cout << "double" nt
+    else if (res2 > int(res2)) cout << "long long" nt
+    else cout << "int" nt
 }
 
 // main function
-int32_t main () {
+int32_t main() {
     Code By Vaibhav
     // int Bhai_Test_Case_Hai_Ya; cin >> Bhai_Test_Case_Hai_Ya; while (Bhai_Test_Case_Hai_Ya-- > 0)
-        Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
+    Solve_Karo_Jaldi_Sa_Dusra_Bhi_Karna_Hai();
 
     return 0;
 }
