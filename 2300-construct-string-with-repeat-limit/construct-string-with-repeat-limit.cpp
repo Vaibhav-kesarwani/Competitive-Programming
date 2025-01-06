1348f1a61d15011bd7fc26095bccc9184356ed89
Vaibhav Kesarwani <116189379+Vaibhav-kesarwani@users.noreply.github.com>
2025-01-06 18:45:59 +0530

    Time: 10 ms (90.17%) | Memory: 28.1 MB (66.00%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -1,37 +1,49 @@
 class Solution {
 public:
     string repeatLimitedString(string s, int repeatLimit) {
-        sort(s.rbegin(), s.rend());
-
-        string result;
-        int freq = 1;
-        int pointer = 0;
-
-        for (int i = 0; i < s.size(); ++i) {
-            if (!result.empty() && result.back() == s[i]) {
-                if (freq < repeatLimit) {
-                    result += s[i];
-                    freq++;
-                } else {
-                    pointer = max(pointer, i + 1);
-                    while (pointer < s.size() && s[pointer] == s[i]) {
-                        pointer++;
-                    }
-
-                    if (pointer < s.size()) {
-                        result += s[pointer];
-                        swap(s[i], s[pointer]);
-                        freq = 1;
-                    } else {
-                        break;
-                    }
-                }
-            } else {
-                result += s[i];
-                freq = 1;
-            }
+        std::string result;
+        int symbolCount[26] = {0};
+        int indices[26] = {0};
+        
+        for (char c : s) { // Count total number of symbols in s
+            symbolCount[c - 'a']++;
         }
 
-        return result;
+
+        for (int i = 1; i < 26; i++) { // Setup indices
+            if (symbolCount[i] > 0)
+                indices[i] = i;
+            else
+                indices[i] = indices[i - 1];
+        }
+
+        int i = 25;
+        while (true) { // Loop for every letter 'z'->'a'
+            if (symbolCount[i] <= 0) { // Ran out of symbols
+                i = indices[i - 1]; // Pick the next present llc
+            }
+
+            if (i == 0) { // i points to 'a', so we can no longer take llc from the left
+                result.append(std::min(repeatLimit, symbolCount[0]), 'a'); // Add whatever is left
+                return result;
+            }
+
+            while (symbolCount[i] > repeatLimit) { // Handle the case when we have more symbols than repeatLimit
+                result.append(repeatLimit, 'a' + i);  // Add the character
+                symbolCount[i] -= repeatLimit;
+
+                if (symbolCount[indices[i - 1]] <= 0) { // If next llc is empty
+                    if (indices[i - 1] <= 0) // If indices[i-1] points to 'a' => can't take the next llc
+                        return result;
+                    indices[i - 1] = indices[indices[i - 1] - 1]; // Move to the next lexicographically largest
+                }
+
+                result.push_back('a' + indices[i - 1]); // Add "filler" character
+                symbolCount[indices[i - 1]]--;
+            }
+
+            result.append(symbolCount[i], 'a' + i); // Add remaining characters
+            symbolCount[i] = 0;
+        }
     }
 };
