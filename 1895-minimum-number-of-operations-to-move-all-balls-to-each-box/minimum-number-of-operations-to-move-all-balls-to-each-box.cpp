74b8bf04cb74192383c20e66e75ffff08815672c
Vaibhav Kesarwani <116189379+Vaibhav-kesarwani@users.noreply.github.com>
2025-01-06 18:29:41 +0530

    Time: 1 ms (68.94%) | Memory: 12.8 MB (8.62%) - LeetSync

---
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+         int n = boxes.length();
+    vector<int> distances(n, 0);
+
+    int prefixCount = 0;  
+    int prefixSum = 0;    
+
+    for (int i = 0; i < n; ++i) {
+        distances[i] = prefixCount * i - prefixSum;
+        if (boxes[i] == '1') {
+            ++prefixCount;
+            prefixSum += i;
+        }
+    }
+
+    int suffixCount = 0;  
+    int suffixSum = 0;    
+
+    for (int i = n - 1; i >= 0; --i) {
+        distances[i] += suffixSum - suffixCount * i;
+        if (boxes[i] == '1') {
+            ++suffixCount;
+            suffixSum += i;
+        }
+    }
+
+    vector<int> ans;
+    for (int dist : distances) {
+       ans.push_back(dist);
+    }
+
+    return ans;
+    }
+};
