0e73d8677f637334d45d650339cd727576d1d926
Vaibhav Kesarwani <116189379+Vaibhav-kesarwani@users.noreply.github.com>
2025-01-08 09:42:27 +0530

    Time: 7 ms (33.69%) | Memory: 23.6 MB (66.95%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    bool isPrefixAndSuffix(const string& str1, const string& str2) {
-        int n1 = str1.size(), n2 = str2.size();
-        if (n1 > n2)
-            return false;
-        return str2.substr(0, n1) == str1 && str2.substr(n2 - n1) == str1;
+    int countPrefixSuffixPairs(vector<string>& words) {
+        int length = words.size();
+        
+        int numPairs = 0;
+        for (int i = 0; i < length; ++i) {
+            for (int j = i + 1; j < length; ++j)
+                numPairs += isPrefixSuffix(words[i], words[j]);
+        }
+
+        return numPairs;        
     }
 
-    int countPrefixSuffixPairs(vector<string>& words) {
-        int n = words.size(), count = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (isPrefixAndSuffix(words[i], words[j])) {
-                    ++count;
-                }
-            }
-        }
-        return count;
+    bool isPrefixSuffix(string word1, string word2) {
+        int length1 = word1.length(), length2 = word2.length();
+
+        if (length1 > length2)
+            return false;
+
+        bool flag = true;
+        for (int l1 = 0, r1 = length1 - 1, l2 = 0, r2 = length2 - 1; l1 < length1 && r1 >= 0 && l2 < length2 && r2 >= 0 && flag; ++l1, --r1, ++l2, --r2)
+            flag = (word1[l1] == word2[l2]) && (word1[r1] == word2[r2]);
+        
+        return flag;
     }
 };
