70387474e0dbfe4f531dddfdfa97c57bc88445fc
Vaibhav Kesarwani <116189379+Vaibhav-kesarwani@users.noreply.github.com>
2025-01-15 09:31:37 +0530

    Time: 0 ms (100.00%) | Memory: 24.2 MB (84.53%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
@@ -6,38 +6,52 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> findMode(TreeNode* root) {
-        unordered_map<int, int> counts;
-        vector<int> modes;
-        int maxCount = 0;
-
-        inorder(root, counts, maxCount, modes);
-
-        return modes;        
+        vector<int> res;
+        if (!root->left && !root->right) {
+            return {root->val};
+        }
+        int f = 0, num = -1, maxf = 0;
+        inorder(res, f, root, num, maxf);
+        if (f > maxf) {
+            return {num};
+        } 
+        else if (f == maxf) {
+            res.push_back(num);
+        }
+        return res;
     }
 
-    void inorder(TreeNode* node, unordered_map<int, int>& counts, int& maxCount, vector<int>& modes) {
-        if (node == nullptr) {
+    void inorder(vector<int>& res, int& f, TreeNode* root, int& num,
+                 int& maxf) {
+        if (!root)
             return;
+        inorder(res, f, root->left, num, maxf);
+        if (f == 0) {
+            f = 1;
+            num = root->val;
+        } else {
+            if (root->val == num) {
+                f++;
+            } else {
+                if (f > maxf) {
+                    res.clear();
+                    res.push_back(num);
+                    maxf = f;
+                } 
+                else if (f == maxf) {
+                    res.push_back(num);
+                }
+                f = 1;
+                num = root->val;
+            }
         }
-
-        inorder(node->left, counts, maxCount, modes);
-
-        int count = ++counts[node->val];
-
-        if (count > maxCount) {
-            maxCount = count;
-            modes.clear();
-            modes.push_back(node->val);
-        } else if (count == maxCount) {
-            modes.push_back(node->val);
-        }
-
-        inorder(node->right, counts, maxCount, modes);
+        inorder(res, f, root->right, num, maxf);
     }
 };
