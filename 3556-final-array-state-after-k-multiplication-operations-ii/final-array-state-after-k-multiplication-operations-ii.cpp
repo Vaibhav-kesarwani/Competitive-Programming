5cb195b9d39afe1b75e5872bad9c66418e964744
Vaibhav Kesarwani <116189379+Vaibhav-kesarwani@users.noreply.github.com>
2025-01-15 21:45:56 +0530

    Time: 7 ms (99.56%) | Memory: 53.6 MB (84.10%) - LeetSync

--- 3556-final-array-state-after-k-multiplication-operations-ii/final-array-state-after-k-multiplication-operations-ii.cpp
+++ 3556-final-array-state-after-k-multiplication-operations-ii/final-array-state-after-k-multiplication-operations-ii.cpp
@@ -1,58 +1,64 @@
-const long long mod = 1000000007;
-
 class Solution {
-public:
-    long long power_mod(long long base, long long exp, long long mod) {
-    long long result = 1;
-    
-    while (exp > 0) {
-        if (exp % 2 == 1) {
-            result = (result * base) % mod;
+    const int MOD = 1'000'000'007;
+
+    long long pow(long long x, int n) {
+        long long res = 1;
+        for (; n; n /= 2) {
+            if (n % 2) {
+                res = res * x % MOD;
+            }
+            x = x * x % MOD;
         }
-        base = (base * base) % mod;
-        exp = exp / 2;
+        return res;
     }
-    // this function multiplies a number by itself n times in O(logn)
-    return result;
-}
+
+public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        if (multiplier == 1) return nums; // do not miss this!!
-        priority_queue <pair<long long,long long>, vector<pair<long long,long long>>, greater<pair<long long,long long>>> pq;
+        if (multiplier == 1) { 
+            return move(nums);
+        }
+
         int n = nums.size();
-        for (int i = 0; i < n; i++) pq.push({nums[i],i});
-        unordered_map<int,int> m,m1;
-        // m for storing number of operations at each index
-        //m1 to know if all elements have got an operation
-        while (1){
-            if ((int)m1.size() == n || k == 0) break; // no more operations or state achieved
-            long long x = pq.top().first, y = pq.top().second;
-            pq.pop();
-            x *= multiplier; pq.push({x,y});
-            m1[y]++; k--;
+        long long mx = ranges::max(nums);
+        vector<pair<long long, int>> h(n);
+        for (int i = 0; i < n; i++) {
+            h[i] = {nums[i], i};
         }
-        vector<long long> v(n); // to avoid integer overflow
-        // This vector will store the elements after ideal state is achieved
-        while (!pq.empty()){
-            long long x = pq.top().first, y = pq.top().second;
-            v[y] = x;
-            pq.pop();
+        auto clone = h;
+
+        int left = k;
+        for (auto& [x, _] : h) {
+            while (x < mx) {
+                x *= multiplier;
+                if (--left < 0) {
+                    goto outer;
+                }
+            }
         }
-        int rep = k/n, md = k%n;
-        for (int i = 0; i < n; i++) pq.push({v[i],i});
-        // I had to push it again because I did not declare v initially due to contest pressure. Better to do that.
-        while (!pq.empty()){
-            int x = pq.top().second;
-            m[x] = rep;
-            if (md > 0)m[x]++,md--;
-            // index x has been assigned its number of operations
-            pq.pop();
+        outer:;
+
+        if (left < 0) {
+            h = move(clone);
+            ranges::make_heap(h, greater<>()); 
+            while (k--) {
+                ranges::pop_heap(h, greater<>());
+                h.back().first *= multiplier;
+                ranges::push_heap(h, greater<>());
+            }
+            for (auto& [x, j] : h) {
+                nums[j] = x % MOD;
+            }
+            return move(nums);
         }
-        // Now just compute every value while calling power_mod funtion
-        for (int i = 0; i < n; i++){
-            long long mlt = power_mod(multiplier, m[i],mod);
-            v[i] = ((v[i]%mod)*(mlt%mod))%mod;
-            nums[i] = v[i];
+
+        k = left;
+        long long pow1 = pow(multiplier, k / n);
+        long long pow2 = pow1 * multiplier % MOD;
+        ranges::nth_element(h, h.begin() + k % n);
+        for (int i = 0; i < n; i++) {
+            auto& [x, j] = h[i];
+            nums[j] = x % MOD * (i < k % n ? pow2 : pow1) % MOD;
         }
-        return nums;
+        return move(nums);
     }
 };
