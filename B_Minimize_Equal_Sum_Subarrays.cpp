90b5eb75ab76ad5447119b88c015f32fb285d563
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-01-17 14:56:41 +0530

    CP

---
+++ B_Minimize_Equal_Sum_Subarrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	while(t--){
+		int x, y, k; cin >> x >> y >> k;
+		for(int i = 0; i < k - k % 2; i++){
+			cout << x - (i & 1 ? 1 : -1) * (i / 2 + 1) << " " << y << "\n";
+		} 
+		if(k & 1){
+			cout << x << " " << y << "\n";
+		}
+	}
+}
