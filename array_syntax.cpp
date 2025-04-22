3eb320ea200735450b145747007f36b7705e9f88
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-04-22 18:55:48 +0530

    CP

---
+++ array_syntax.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int marks[5] = {10, 20, 30, 40, 50};
+    int price[] = {10, 33, 44};
+
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+
+    return 0;
+}
