8cb0484a1d78478fd34b2a1dbd7f02fd33b9fc34
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2026-04-11 22:43:37 +0530

    contest

---
+++ A_illegal.cpp
@@ -0,0 +1,81 @@
+/*
+ * Author        :         vaibhav_404
+ * Date          :         11-04-2026
+ * Time          :         21:50
+*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Shorthand Macros
+#define pb push_back
+#define fi first
+#define se second
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define lcm(a,b) (a*(b/gcd(a,b)))
+#define sz(a) int((a).size())
+#define tr(c,i) for(__typeof__((c)).begin() i = (c).begin(); i != (c).end(); i++)
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+ 
+// Functions
+int gcd(int a, int b) {
+    while (b != 0) {
+        a = a % b;
+        swap(a, b);
+    }
+    return a;
+}
+ 
+// Fast IO
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(0);
+#define Vaibhav cout.tie(0);
+ 
+// Type aliases
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+using vi = vector<int>;
+using vll = vector<ll>;
+using vc = vector<char>;
+using vs = vector<string>;
+
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vpii = vector<pii>;
+
+using si = set<int>;
+using sll = set<ll>;
+using usi = unordered_set<int>;
+using usll = unordered_set<ll>;
+
+using mii = map<int,int>;
+using mll = map<ll,ll>;
+using umii = unordered_map<int,int>;
+using umll = unordered_map<ll,ll>;
+ 
+template<typename T>
+istream& operator>>(istream &in, vector<T> &v) { for (auto &x : v) in >> x; return in; }
+ 
+template<typename T>
+ostream& operator<<(ostream &out, const vector<T> &v) { for (const auto &x : v) out << x << " "; return out; }
+ 
+void solve() {
+    string s; cin >> s;
+
+    cout << (sz(s) % 5 == 0 ? "Yes" : "No") << endl;
+}
+ 
+signed main() {
+    Code By Vaibhav
+    // int t; cin >> t; while (t-- > 0)
+        solve();
+    return 0;
+}
