f50b36f4f7e7b82179eccaee39997744b942bfca
Vaibhav Kesarwani <116189379+Vaibhav-kesarwani@users.noreply.github.com>
2026-09-04 18:58:10 +0530

    [3903] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ Leetcode/DCP-09-26/3903-smallest-stable-index-i/3903-Smallest-Stable-Index-I.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        vector<int> prefix(n), suffix(n);
+
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = max(prefix[i - 1], nums[i]);
+        }
+
+        suffix[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = min(suffix[i + 1], nums[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if ((prefix[i] - suffix[i]) <= k) return i;
+        }
+
+        return -1;
+    }
+};
